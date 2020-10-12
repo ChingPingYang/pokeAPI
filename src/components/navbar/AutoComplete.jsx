f0c56c0bd1d3c6d6827d4058be80fb09f38f277d
JamesYang <gn00999829@gmail.com>
2020-10-12 00:28:55 -0700

    capitalize text in search box

--- src/components/navbar/AutoComplete.jsx
+++ src/components/navbar/AutoComplete.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
+import { capitalize } from '../../util/utilFuns';
 import { media } from '../../style/media';
  
 const AutoComplete = ({data, reset}) => {
     const history = useHistory();
     
     const handleOnclick = (e) => {
-        const name = e.target.innerHTML;
+        const name = e.target.innerHTML.toLowerCase();
         const pokemon = data.filter( i => i.name === name);
         if(pokemon.length < 1) return history.push('/404');
         let url = pokemon[0].url.replace('https://pokeapi.co/api/v2/','');
@@ -20,7 +21,7 @@ const AutoComplete = ({data, reset}) => {
                 {data.map((pokemon, index) => {
                     return (
                         <div className="option" key={index}>
-                            <label htmlFor={pokemon.name} onClick={handleOnclick} >{pokemon.name}</label>
+                            <label htmlFor={pokemon.name} onClick={handleOnclick} >{capitalize(pokemon.name)}</label>
                             <input type="radio" name="autocomplete" id={pokemon.name} />
                         </div>
                     )
